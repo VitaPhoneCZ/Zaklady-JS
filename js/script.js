742da5c64a0442c33e752712afefff36854718d5
Vita_ROG_Ally <vitek.f18@gmail.com>
2024-09-13 11:10:49 +0200

    update

--- js/script.js
+++ js/script.js
@@ -3,13 +3,18 @@ let i = 1;
 function add(){
     let input = document.getElementById("input");
     let output = document.getElementById("output");
-    /*document.getElementById("output").innerHTML = input.value;*/
-
-    /*document.getElementById("output").innerHTML = document.getElementById("input").value;*/
-    
     console.log("Hodnota inputu je: ", input)
     console.log("Hodnota outputu je: ", output)
-
     output.innerHTML += i + ". " + input.value + "<br>";
     i++;
+}
+function add2() {
+    let lastMessage = output.innerHTML.split("<br>").slice(-2, -1)[0]; // Získá poslední zprávu
+    output.innerHTML = output.innerHTML.replace(lastMessage, lastMessage + "💀"); // Přidá emoji
+}
+
+function del() {
+    let output = document.getElementById("output");
+    output.innerHTML = ""; // Smaže všechny zprávy
+    i=1;
 }
