8fd54fac58b6d39b58b1d340b84fd0fa33cbcfd2
Vita_ROG_Ally <vitek.f18@gmail.com>
2024-09-20 11:04:16 +0200

    console log update

--- js/img.js
+++ js/img.js
@@ -3,12 +3,14 @@ var i=1;
 function img() {
     var imgURL = document.getElementById("input").value;
     let imgID = document.getElementById("imgID");
+    console.log("Hodnota imgURL je: ", imgURL);
+    console.log("Hodnota imgID je: ", imgID);
     imgID.src = imgURL;
 
     let input = document.getElementById("name_input");
     let output = document.getElementById("output");
-    console.log("Hodnota inputu je: ", name_input)
-    console.log("Hodnota outputu je: ", output)
+    console.log("Hodnota inputu je: ", name_input);
+    console.log("Hodnota outputu je: ", output);
     output.innerHTML += i + ". " + input.value + "<br>";
     i++;
 }
